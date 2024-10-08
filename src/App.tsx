d845936cfe4c63f1c296378056fe4750d86dd997
sangatsharma <sangatsharma2@gmail.com>
2024-10-08 20:55:27 +0545

    initialize repository

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-full  h-screen flex items-center justify-center">
+      <h1 className="text-red-500 text-[4rem]">Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
